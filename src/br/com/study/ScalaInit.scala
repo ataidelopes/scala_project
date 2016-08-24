4d8edf2e21c4685cc122eae90c1dece942b1b4cc
ruby --version <ataidelopes@gmail.com>
2016-08-24 14:36:35 -0300

    adicionando os codigos do estudo scala para o github (carga inicial)

---
+++ src/br/com/study/ScalaInit.scala
@@ -0,0 +1,19 @@
+package br.com.study
+
+object ScalaInit extends App{
+  
+  def fibo(x:Int): BigInt = {
+    var a:BigInt = 0
+    var b:BigInt = 1
+    var c:BigInt = 0
+    
+    for (_ <- 1 to x) {
+      c = a + b
+      a = b
+      b = c
+    }
+      return a
+  }
+  println(fibo(100)+" | ")
+  
+}
