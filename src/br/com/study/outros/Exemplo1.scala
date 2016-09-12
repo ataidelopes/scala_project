aaf0911c4d1b6e709902af89a9a123cc71e854c1
ruby --version <ataidelopes@gmail.com>
2016-09-12 16:51:54 -0300

    a

---
+++ src/br/com/study/outros/Exemplo1.scala
@@ -0,0 +1,20 @@
+package br.com.study.outros
+
+object Exemplo1 {
+    
+    /* *
+     * este é o primeiro programa script scala
+     * */
+     
+     def main(args: Array[String]): Unit = {
+         var myVar : Int = 10
+         val myVal : String = "test in assign on variable"
+         var myvar1 = 20
+         val myVal2 = "teste in assign val 2"
+         
+         println(myVar)
+         println(myVal)
+         println(myvar1)
+         println(myVal2)
+     }
+}
